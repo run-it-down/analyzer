8d3d0a80e7e9431db4100f8fb49ef073a9532e25
zkdev <dev@zeekay.dev>
2020-10-30 19:33:27 +0100

    Init analyzer

---
+++ src/api.py
@@ -0,0 +1,39 @@
+import json
+
+import falcon
+
+import analysis
+import database
+import util
+
+
+logger = util.Logger(__name__)
+
+
+class Winrate:
+    def on_get(self, req, resp):
+        logger.info('/GET winrate')
+        params = req.params
+        conn = database.get_connection()
+        summoner = database.select_summoner(conn=conn,
+                                            summoner_name=params['summoner'],
+                                            )
+        wr = analysis.get_winrate(summoner=summoner,
+                                  conn=conn,
+                                  )
+        resp.body = json.dumps(wr)
+
+
+def create():
+    api = falcon.API()
+    api.add_route('/winrate', Winrate())
+    logger.info('falcon initialized')
+
+    conn = database.get_connection()
+    database.kill_connection(conn)
+    logger.info('database is ready')
+
+    return api
+
+
+application = create()
